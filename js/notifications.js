a5b66acf28647116a9a45e4e44b235d7bc5e0d73
Yein Aria Kim <yein6227@gmail.com>
2026-09-17 13:24:10 +0900

    Update notifications.js

--- js/notifications.js
+++ js/notifications.js
@@ -6,7 +6,7 @@
 import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-messaging.js";
 
 // Firebase 콘솔 > 프로젝트 설정 > 클라우드 메시징 > 웹 푸시 인증서(VAPID 키)에서 발급
-const VAPID_KEY = "YOUR_VAPID_KEY";
+const VAPID_KEY = "BPLcQ6tzyrBK0jYc625TDyqmgWxdPIMKp-HS1fWgyNUUzpG_JcNjtT-L4ENtJKNQyL_qS2jCdphx42UX8e3asGQ";
 
 export async function setupNotifications(app, db, docFns, uid) {
   if (!("Notification" in window) || !("serviceWorker" in navigator)) return;
